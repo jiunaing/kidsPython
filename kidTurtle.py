4dcbd67a91783aafe7155de06fee863955bd0c09
jiunaing <jiunaing@yahoo.com>
2023-03-04 08:27:51 +0800

    Create kidTurtle.py

---
+++ kidTurtle.py
@@ -0,0 +1,24 @@
+import turtle
+
+wn = turtle.Screen()
+wn.bgcolor("light green")
+wn.title("Turtle")
+myPen = turtle.Turtle()
+
+'''
+myPen = turtle.Turtle().pencolor("white")
+myPen.pencolor("red")
+myPen.goto(100,100)
+myPen.penup()
+myPen.pendown()
+
+myPen.forward(200)
+myPen.backward(300)
+
+
+
+wn.clearscreen()
+'''
+skk.forward(100)
+
+#turtle.done()
